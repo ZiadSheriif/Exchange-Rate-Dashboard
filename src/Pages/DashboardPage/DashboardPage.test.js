265cb07cb071f7f2c5d794813ee285f92d3a347e
ZiadSheriif <zsherif308@gmail.com>
2024-03-31 19:30:30 +0200

    test: Add test coverage

--- src/Pages/DashboardPage/DashboardPage.test.js
+++ src/Pages/DashboardPage/DashboardPage.test.js
@@ -3,18 +3,6 @@ import { render, screen } from "@testing-library/react";
 import DashboardPage from "./DashboardPage";
 
 describe("DashboardPage", () => {
-  // test('renders without crashing', () => {
-  //   render(<DashboardPage mode="view" />);
-  // });
-
-  // test('renders the correct elements in "view" mode', () => {
-  //   render(<DashboardPage mode="view" />);
-  //   expect(screen.queryByText('Show Line Chart')).toBeNull();
-  //   expect(screen.queryByText('Show Bar Chart')).toBeNull();
-  //   expect(screen.queryByText('Hide Line Chart')).toBeNull();
-  //   expect(screen.queryByText('Hide Bar Chart')).toBeNull();
-  // });
-
   test('renders the correct elements in "edit" mode', () => {
     render(<DashboardPage mode="edit" />);
     expect(screen.getByText("Hide Line Chart")).toBeInTheDocument();
